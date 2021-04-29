148978898ed65dbb9fba443d6994733f44c8ec1c
joshua-gibson <joshua.gibson.dev@gmail.com>
2021-04-29 11:32:05 +0100

    threejs loading

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", function (request, res) {
+  res.contentType("html");
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/main.js", function (req, res) {
+  res.sendFile(__dirname + "/main.js");
+});
+
+app.get("/lib/three.js", function (req, res) {
+  res.sendFile(__dirname + "/lib/three.js");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
